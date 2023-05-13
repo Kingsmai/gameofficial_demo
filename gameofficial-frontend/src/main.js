2936785a7c6d9c8eeec0d1cf32c2d85641269002
Xiaomai <xsbugh@gmail.com>
2023-05-13 10:28:47 +0800

    曝露给局域网链接

--- gameofficial-frontend/src/main.js
+++ gameofficial-frontend/src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
@@ -11,6 +12,7 @@ import axios from "axios";
 
 const app = createApp(App)
 
+// axios.defaults.baseURL = 'http://192.168.1.100:8080'
 axios.defaults.baseURL = 'http://localhost:8080'
 
 app.use(createPinia())
